64404a147be0f5d2664b3e200d42c19f7a722006
shotaIDE <ide.shota.dev@gmail.com>
2022-03-08 23:05:30 +0900

    fix: 音声のピックに修正

--- lib/ui/select_sounds_screen.dart
+++ lib/ui/select_sounds_screen.dart
@@ -310,8 +310,7 @@ class _SelectTemplateState extends ConsumerState<SelectSoundsScreen> {
 
   Future<void> _selectSound({required PlayerChoiceSound target}) async {
     final result = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
-      allowedExtensions: ['wav', 'm4a', 'mp3'],
+      type: FileType.audio,
     );
 
     if (result == null) {
