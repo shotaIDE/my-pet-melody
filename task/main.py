b73205c0cbce74bd9a7f280756d4aa526a72c80b
shotaIDE <ide.shota.dev@gmail.com>
2022-05-22 14:34:25 +0900

    feat: Cloud Functionsにデプロイできる時点でコミット

--- task/main.py
+++ task/main.py
@@ -5,6 +5,7 @@ import os
 import statistics
 from datetime import datetime
 
+import functions_framework
 from flask import Flask, request, url_for
 from pydub import AudioSegment, silence
 
@@ -175,3 +176,23 @@ def detect_non_silence():
         'segments': non_silences_list_raw[target_index]['segments'],
         'durationMilliseconds': duration_milliseconds,
     }
+
+
+# [START functions_helloworld_get]
+@functions_framework.http
+def hello_get(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    Note:
+        For more information on how Flask integrates with Cloud
+        Functions, see the `Writing HTTP functions` page.
+        <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
+    """
+    return 'Hello World!'
+# [END functions_helloworld_get]
