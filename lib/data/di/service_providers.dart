e3e5081b354c38e1d6ecea9cc2f6fff7e24b9ddc
shotaIDE <ide.shota.dev@gmail.com>
2022-05-27 20:28:05 +0900

    fix: remove unnecessary conditions

--- lib/data/di/service_providers.dart
+++ lib/data/di/service_providers.dart
@@ -5,28 +5,17 @@ import 'package:meow_music/data/service/database_service_firebase.dart';
 import 'package:meow_music/data/service/push_notification_service.dart';
 import 'package:meow_music/data/service/storage_service.dart';
 import 'package:meow_music/data/service/storage_service_firebase.dart';
-import 'package:meow_music/flavor.dart';
 
 final authServiceProvider = Provider(
   (_) => AuthService(),
 );
 
 final databaseServiceProvider = Provider<DatabaseService>(
-  (_) {
-    if (F.flavor == Flavor.local) {
-      return DatabaseServiceFirebase();
-    }
-    return DatabaseServiceFirebase();
-  },
+  (_) => DatabaseServiceFirebase(),
 );
 
 final storageServiceProvider = Provider<StorageService>(
-  (_) {
-    if (F.flavor == Flavor.local) {
-      return StorageServiceFirebase();
-    }
-    return StorageServiceFirebase();
-  },
+  (_) => StorageServiceFirebase(),
 );
 
 final pushNotificationServiceProvider = Provider(
