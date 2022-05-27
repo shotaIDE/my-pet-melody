f8e8fab44b7a8535c0e09ea4aae04e13946130b3
shotaIDE <ide.shota.dev@gmail.com>
2022-05-27 23:37:33 +0900

    chore: add other configurations

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,28 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meow_music/data/definitions/app_definitions.dart';
-import 'package:meow_music/firebase_options_dev.dart';
+import 'package:meow_music/firebase_options_dev.dart' as dev;
+import 'package:meow_music/firebase_options_emulator.dart' as emulator;
+import 'package:meow_music/firebase_options_local.dart' as local;
 import 'package:meow_music/flavor.dart';
 import 'package:meow_music/root_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  final FirebaseOptions options;
+  switch (F.flavor) {
+    case Flavor.local:
+      options = local.DefaultFirebaseOptions.currentPlatform;
+      break;
+    case Flavor.emulator:
+      options = emulator.DefaultFirebaseOptions.currentPlatform;
+      break;
+    case Flavor.dev:
+      options = dev.DefaultFirebaseOptions.currentPlatform;
+      break;
+  }
+  await Firebase.initializeApp(options: options);
 
   if (F.flavor == Flavor.emulator) {
     const serverHost = AppDefinitions.serverHost;
