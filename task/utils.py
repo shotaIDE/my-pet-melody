07c658a4c7ef2e813bd9a63465de15a0afac4c4e
shotaIDE <ide.shota.dev@gmail.com>
2022-05-29 03:17:23 +0900

    refactor: define the function to detect

--- task/utils.py
+++ task/utils.py
@@ -4,6 +4,7 @@ import os
 import statistics
 import time
 from datetime import datetime
+from typing import Optional
 
 from pydub import AudioSegment, silence
 
@@ -30,44 +31,20 @@ def detect_non_silence(store_path: str) -> dict:
 
     normalized_sound = sound.normalize(headroom=1.0)
 
-    non_silences_list_raw: dict[int, list[list[int]]] = {
+    non_silences_list: dict[int, list[list[int]]] = {
         threshould: silence.detect_nonsilent(
             normalized_sound, silence_thresh=threshould)
         for threshould in range(-30, -5, 5)
     }
 
-    non_silences_list = {
-        threshould: non_silences
-        for threshould, non_silences in non_silences_list_raw.items()
-        if len(non_silences) > 0
-    }
+    target_threshould = _find_by_segments_duration_meanings(
+        candidates=non_silences_list
+    )
 
-    # 1000ms との差分の平均が小さい順にソートし、それを候補とする
-
-    non_silences_duratioins = {
-        threshould: [
-            non_silence[1] - non_silence[0]
-            for non_silence in non_silences
-        ]
-        for threshould, non_silences in non_silences_list.items()
-    }
-
-    average_list = {
-        threshould: statistics.mean(non_silences_duration)
-        for threshould, non_silences_duration
-        in non_silences_duratioins.items()
-    }
-
-    sorted_average_list = sorted(
-        average_list.items(),
-        key=lambda x: x[1])
-
-    if len(sorted_average_list) > 0:
-        target_threshould = sorted_average_list[0][0]
-
-        segments = non_silences_list_raw[target_threshould]
-    else:
+    if target_threshould is None:
         segments = []
+    else:
+        segments = non_silences_list[target_threshould]
 
     result = {
         'segments': segments,
@@ -115,3 +92,38 @@ def generate_piece(
     normalized_overlayed.export(export_path)
 
     return export_path
+
+
+def _find_by_segments_duration_meanings(
+    candidates: dict[int, list[list[int]]]
+) -> Optional[int]:
+    some_detected_list = {
+        threshould: non_silences
+        for threshould, non_silences in candidates.items()
+        if len(non_silences) > 0
+    }
+
+    # 1000ms との差分の平均が小さい順にソートし、それを候補とする
+
+    durations = {
+        threshould: [
+            non_silence[1] - non_silence[0]
+            for non_silence in non_silences
+        ]
+        for threshould, non_silences in some_detected_list.items()
+    }
+
+    averages_of_durations = {
+        threshould: statistics.mean(non_silences_duration)
+        for threshould, non_silences_duration
+        in durations.items()
+    }
+
+    sorted_averages_of_durations = sorted(
+        averages_of_durations.items(),
+        key=lambda x: x[1])
+
+    if len(sorted_averages_of_durations) == 0:
+        return None
+
+    return sorted_averages_of_durations[0][0]
