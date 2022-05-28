e72efaa875796a70795d61095c454da3d9e2085c
shotaIDE <ide.shota.dev@gmail.com>
2022-05-29 03:05:08 +0900

    refactor: add Python typing

--- task/utils.py
+++ task/utils.py
@@ -2,6 +2,7 @@
 
 import os
 import statistics
+import time
 from datetime import datetime
 
 from pydub import AudioSegment, silence
@@ -20,69 +21,72 @@ def generate_store_file_name(file_name: str) -> tuple[str, str]:
 
 
 def detect_non_silence(store_path: str) -> dict:
+    start_time = time.perf_counter()
+
     sound = AudioSegment.from_file(store_path)
 
-    duration_seconds = sound.duration_seconds
+    duration_seconds: int = sound.duration_seconds
     duration_milliseconds = int(round(duration_seconds, 3) * 1000)
 
     normalized_sound = sound.normalize(headroom=1.0)
 
-    non_silences_list_raw = [
-        {
-            'segments': silence.detect_nonsilent(
-                normalized_sound, silence_thresh=threshould),
-            'threshould': threshould,
-        }
-        for threshould in range(-30, -10)
-    ]
+    non_silences_list_raw: dict[int, list[list[int]]] = {
+        threshould: silence.detect_nonsilent(
+            normalized_sound, silence_thresh=threshould)
+        for threshould in range(-30, -5, 5)
+    }
 
-    non_silences_list = [
-        non_silences
-        for non_silences in non_silences_list_raw
-        if len(non_silences['segments']) > 0
-    ]
+    non_silences_list = {
+        threshould: non_silences
+        for threshould, non_silences in non_silences_list_raw.items()
+        if len(non_silences) > 0
+    }
 
     # 1000ms との差分の平均が小さい順にソートし、それを候補とする
 
-    non_silences_duratioins = [
-        {
-            'segment_duration_list': [
-                non_silence[1] - non_silence[0]
-                for non_silence in non_silences['segments']
-            ],
-            'threshould': non_silences['threshould'],
-        }
-        for non_silences in non_silences_list
-    ]
+    non_silences_duratioins = {
+        threshould: [
+            non_silence[1] - non_silence[0]
+            for non_silence in non_silences
+        ]
+        for threshould, non_silences in non_silences_list.items()
+    }
 
-    average_list = [
-        {
-            'segment_duration_average':
-                statistics.mean(
-                    non_silences_duration['segment_duration_list']),
-            'threshould': non_silences_duration['threshould'],
-        }
-        for non_silences_duration in non_silences_duratioins
-    ]
+    average_list = {
+        threshould: statistics.mean(non_silences_duration)
+        for threshould, non_silences_duration
+        in non_silences_duratioins.items()
+    }
 
     sorted_average_list = sorted(
-        average_list,
-        key=lambda x: x['segment_duration_average'])
+        average_list.items(),
+        key=lambda x: x[1])
 
-    target_threshould = sorted_average_list[0]['threshould']
+    if len(sorted_average_list) > 0:
+        target_threshould = sorted_average_list[0][0]
 
-    target_index = target_threshould + 30
+        segments = non_silences_list_raw[target_threshould]
+    else:
+        segments = []
 
-    return {
-        'segments': non_silences_list_raw[target_index]['segments'],
+    result = {
+        'segments': segments,
         'durationMilliseconds': duration_milliseconds,
     }
+
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+    print(f'Running time to detect non silence: {elapsed_time:.3f}s')
+
+    return result
 
 
 def generate_piece(
     template_path: str,
     sound_paths: list[str],
     export_base_path: str
+
+
 ) -> str:
     template = AudioSegment.from_file(template_path)
     sounds = [
