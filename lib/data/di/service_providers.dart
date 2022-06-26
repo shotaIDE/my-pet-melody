ff186b7854e10291bd3fc71ff550a1821cbdab4d
shotaIDE <ide.shota.dev@gmail.com>
2022-06-26 21:00:49 +0900

    refactor: fix di expression

--- lib/data/di/service_providers.dart
+++ lib/data/di/service_providers.dart
@@ -4,19 +4,15 @@ import 'package:meow_music/data/service/push_notification_service.dart';
 import 'package:meow_music/data/service/storage_service.dart';
 import 'package:meow_music/data/service/storage_service_firebase.dart';
 import 'package:meow_music/data/service/storage_service_local_flask.dart';
-import 'package:meow_music/flavor.dart';
 
 final storageServiceProvider = Provider<StorageService>(
-  (ref) {
-    if (F.flavor == Flavor.emulator) {
-      // Firebase Emulator の Python クライアントはまだ Storage に対応していないので、
-      // Flask のエンドポイントを利用する
-      return StorageServiceLocalFlask(
-        api: ref.watch(storageApiProvider),
-      );
-    }
-    return StorageServiceFirebase();
-  },
+  (ref) => StorageServiceFirebase(),
+);
+
+final storageServiceFlaskProvider = Provider<StorageService>(
+  (ref) => StorageServiceLocalFlask(
+    api: ref.watch(storageApiProvider),
+  ),
 );
 
 final pushNotificationServiceProvider = Provider(
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meow_music/data/definitions/app_definitions.dart';
+import 'package:meow_music/data/di/service_providers.dart';
 import 'package:meow_music/firebase_options_dev.dart' as dev;
 import 'package:meow_music/firebase_options_emulator.dart' as emulator;
 import 'package:meow_music/flavor.dart';
@@ -24,15 +25,26 @@ Future<void> main() async {
   }
   await Firebase.initializeApp(options: options);
 
+  final List<Override> overrides;
+
   if (F.flavor == Flavor.emulator) {
     const serverHost = AppDefinitions.serverHost;
     await FirebaseAuth.instance.useAuthEmulator(serverHost, 9099);
     FirebaseFirestore.instance.useFirestoreEmulator(serverHost, 8080);
     await FirebaseStorage.instance.useStorageEmulator(serverHost, 9199);
+
+    overrides = [
+      // Firebase Emulator の Python クライアントはまだ Storage に対応していないので、
+      // Flask のエンドポイントを利用する)
+      storageServiceProvider.overrideWithProvider(storageServiceFlaskProvider),
+    ];
+  } else {
+    overrides = [];
   }
 
   runApp(
     ProviderScope(
+      overrides: overrides,
       child: RootApp(),
     ),
   );
