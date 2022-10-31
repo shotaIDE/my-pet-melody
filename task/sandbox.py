7b8ed8e8c8f16e096dc925a65352d5927a4d4f27
shotaIDE <ide.shota.dev@gmail.com>
2022-10-31 23:05:52 +0900

    feat: output movie with only sound

--- task/sandbox.py
+++ task/sandbox.py
@@ -1,8 +1,32 @@
 # coding: utf-8
 
+import ffmpeg
+
 
 def generate_movie():
-    print('test')
+    HEIGHT = 1080
+    LENGTH_SECONDS = 3
+    FRAME_RATE = 30
+
+    background_image = (
+        ffmpeg
+        .input('background.jpg')
+        .filter('scale', -1, HEIGHT)
+    )
+    sound = ffmpeg.input('sound.mp3')
+
+    stream = ffmpeg.output(
+        background_image,
+        sound,
+        'output.mp4',
+        vcodec='libx264',
+        acodec='aac',
+        pix_fmt='yuv420p',
+        t=LENGTH_SECONDS,
+        r=FRAME_RATE,
+    )
+
+    ffmpeg.run(stream)
 
 
 if __name__ == '__main__':
