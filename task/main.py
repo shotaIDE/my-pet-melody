daee4db8dce60f06c4bdc757566e524ef7c1694d
shotaIDE <ide.shota.dev@gmail.com>
2022-11-01 22:16:25 +0900

    feat: generate movie in piece api on GCP

--- task/main.py
+++ task/main.py
@@ -13,7 +13,7 @@ from auth import verify_authorization_header
 from database import set_generated_piece, template_overlays
 from detection import detect_non_silence
 from firebase import initialize_firebase
-from piece import generate_piece_sound
+from piece import generate_piece_movie, generate_piece_sound
 from storage import (TEMPLATE_EXTENSION, TEMPLATE_FILE_NAME,
                      USER_MEDIA_DIRECTORY_NAME)
 
@@ -165,20 +165,48 @@ def piece(request):
     # TODO: ファイルの存在を確認するバリデーションチェック
     # TODO: 鳴き声が2つ存在することを確認するバリデーションチェック
 
-    _, export_local_base_path = tempfile.mkstemp()
+    thumbnail_relative_path = (
+        f'{USER_MEDIA_DIRECTORY_NAME}/{uid}/'
+        f'edited/{sound_base_name}'
+    )
+    sound_blob = bucket.blob(sound_relative_path)
 
-    export_local_path = generate_piece_sound(
+    _, piece_sound_export_local_base_path = tempfile.mkstemp()
+
+    piece_sound_export_local_path = generate_piece_sound(
         template_path=template_local_path,
         sound_paths=sound_local_paths,
         overlays=overlays,
-        export_base_path=export_local_base_path,
+        export_base_path=piece_sound_export_local_base_path,
+    )
+
+    _, thumbnail_local_base_path = tempfile.mkstemp()
+    splitted_thumbnail_name = os.path.splitext(thumbnail_base_name)
+    thumbnail_extension = splitted_thumbnail_name[1]
+    thumbnail_local_path = f'{thumbnail_local_base_path}{thumbnail_extension}'
+
+    thumbnail_relative_path = (
+        f'{USER_MEDIA_DIRECTORY_NAME}/{uid}/'
+        f'edited/{thumbnail_base_name}'
+    )
+    thumbnail_blob = bucket.blob(thumbnail_relative_path)
+
+    thumbnail_blob.download_to_filename(thumbnail_local_path)
+
+    _, piece_movie_export_local_base_path = tempfile.mkstemp()
+
+    piece_movie_export_local_path = generate_piece_movie(
+        thumbnail_path=thumbnail_local_path,
+        piece_sound_path=piece_sound_export_local_path,
+        title=display_name,
+        export_base_path=piece_movie_export_local_base_path,
     )
 
     current = datetime.now()
-    export_base_name = current.strftime('%Y%m%d%H%M%S')
-    splitted_file_name = os.path.splitext(export_local_path)
-    export_extension = splitted_file_name[1]
-    export_file_name = f'{export_base_name}{export_extension}'
+    movie_export_base_name = f'{current.strftime("%Y%m%d%H%M%S")}_movie'
+    splitted_file_name = os.path.splitext(piece_sound_export_local_path)
+    movie_export_extension = splitted_file_name[1]
+    export_file_name = f'{movie_export_base_name}{movie_export_extension}'
 
     export_relative_path = (
         f'{USER_MEDIA_DIRECTORY_NAME}/{uid}/'
@@ -186,7 +214,7 @@ def piece(request):
     )
     export_blob = bucket.blob(export_relative_path)
 
-    export_blob.upload_from_filename(export_local_path)
+    export_blob.upload_from_filename(piece_movie_export_local_path)
 
     set_generated_piece(
         uid=uid,
@@ -234,6 +262,6 @@ def piece(request):
                     failed_tokens))
 
     return {
-        'id': export_base_name,
+        'id': movie_export_base_name,
         'path': export_relative_path,
     }
