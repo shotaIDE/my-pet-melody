123c22b0596a08c69578eda63dd704b780a3e024
shotaIDE <ide.shota.dev@gmail.com>
2023-01-07 17:39:17 +0900

    fix: audioplayer breaking changes

--- lib/ui/select_trimmed_sound_view_model.dart
+++ lib/ui/select_trimmed_sound_view_model.dart
@@ -63,9 +63,9 @@ class SelectTrimmedSoundViewModel
 
   Future<void> setup() async {
     _audioPositionSubscription =
-        _player.onAudioPositionChanged.listen(_onAudioPositionReceived);
+        _player.onPositionChanged.listen(_onAudioPositionReceived);
 
-    _audioStoppedSubscription = _player.onPlayerCompletion.listen((_) {
+    _audioStoppedSubscription = _player.onPlayerComplete.listen((_) {
       _onAudioFinished();
     });
 
@@ -204,7 +204,9 @@ class SelectTrimmedSoundViewModel
 
     _setPlayerChoices(playingList);
 
-    await _player.play(url);
+    final source = UrlSource(url);
+
+    await _player.play(source);
   }
 
   Future<void> stop({required PlayerChoice choice}) async {
