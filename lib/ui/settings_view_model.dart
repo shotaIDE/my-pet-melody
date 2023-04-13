0cf1b43ffffc4782b9eef3bc5761997083476d45
shotaIDE <ide.shota.dev@gmail.com>
2023-04-13 23:28:16 +0900

    fix: delete unused codes

--- lib/ui/settings_view_model.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:meow_music/ui/settings_state.dart';
-
-class SettingsViewModel extends StateNotifier<SettingsState> {
-  SettingsViewModel() : super(const SettingsState());
-}
