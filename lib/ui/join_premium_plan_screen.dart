977f98e2d91fcfc72e98bf49c2ab404ad63965b6
shotaIDE <ide.shota.dev@gmail.com>
2023-04-15 10:32:11 +0900

    fix

--- lib/ui/join_premium_plan_screen.dart
+++ lib/ui/join_premium_plan_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:meow_music/ui/join_premium_plan_state.dart';
@@ -74,25 +76,36 @@ class _JoinPremiumPlanScreenState extends ConsumerState<JoinPremiumPlanScreen> {
       title: Text('自動継続課金について'),
       subtitle: Text('期間終了日の24時間以上前に自動更新の解除を行わない場合、契約期間が自動更新されます。'),
     );
-    const subscriptionDescription2Tile = ListTile(
-      title: Text('確認と解約'),
+    final subscriptionDescription2Tile = ListTile(
+      title: const Text('確認と解約'),
       subtitle: Text(
-        '設定アプリを開き、[ご自分の名前] > [サブスクリプション] > [MeowMusic]の画面から、'
-        '次回の自動更新タイミングの確認や、自動更新の解除ができます。',
+        Platform.isIOS
+            ? '設定アプリを開き、[ご自分の名前] > [サブスクリプション] > [MeowMusic]の画面から、'
+                '次回の自動更新タイミングの確認や、自動更新の解除ができます。'
+            : 'Play ストアアプリを開き、設定 > [定期購入] > [MeowMusic]の画面から、'
+                '次回の自動更新タイミングの確認や、自動更新の解除ができます。',
       ),
     );
-    const subscriptionDescription3Tile = ListTile(
-      title: Text('機種変更時の復元'),
+    final subscriptionDescription3Tile = ListTile(
+      title: const Text('機種変更時の復元'),
       subtitle: Text(
-        '機種変更時には、本画面から以前購入したプランを無料で復元できます。'
-        '購入時と同じApple IDでApp Storeにログインした上で復元してください。',
+        Platform.isIOS
+            ? '機種変更時には、本画面から以前購入したプランを無料で復元できます。'
+                '購入時と同じ Apple ID で App Store にログインした上で復元してください。'
+            : '機種変更時には、本画面から以前購入したプランを無料で復元できます。'
+                '購入時と同じ Google アカウントで Play ストアにログインした上で復元してください。',
       ),
     );
-    const subscriptionDescription4Tile = ListTile(
-      title: Text('注意点'),
+    final subscriptionDescription4Tile = ListTile(
+      title: const Text('注意点'),
       subtitle: Text(
-        'アプリ内で課金された方は上記以外の方法での解約できません。当月分のキャンセルについては受け付けておりません。'
-        'Apple IDアカウントを経由して課金されます。',
+        Platform.isIOS
+            ? 'アプリ内で課金された方は上記以外の方法での解約できません。'
+                '当月分のキャンセルについては受け付けておりません。'
+                'Apple ID アカウントを経由して課金されます。'
+            : 'アプリ内で課金された方は上記以外の方法での解約できません。'
+                '当月分のキャンセルについては受け付けておりません。'
+                'Google アカウントを経由して課金されます。',
       ),
     );
 
