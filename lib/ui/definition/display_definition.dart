92f736e9be9cabaf88c4536fb98cb17879e8ee53
Shota Ide <shotaIDE@users.noreply.github.com>
2023-05-18 00:03:59 +0900

    fix: brush up design on home screen (#234)

--- lib/ui/definition/display_definition.dart
+++ lib/ui/definition/display_definition.dart
@@ -1,5 +1,7 @@
 class DisplayDefinition {
   static const aspectRatio = 1.5;
   static const equallyDividedCount = 10;
-  static const cornerRadiusSize = 24.0;
+  static const screenPaddingSmall = 8.0;
+  static const cornerRadiusSizeLarge = 24.0;
+  static const cornerRadiusSizeSmall = 8.0;
 }
--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -50,35 +50,42 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
     } else {
       if (pieces.isNotEmpty) {
         body = ListView.separated(
-          padding: const EdgeInsets.only(bottom: LyingDownCatImage.height),
+          padding: const EdgeInsets.only(
+            top: DisplayDefinition.screenPaddingSmall,
+            bottom: LyingDownCatImage.height,
+            left: DisplayDefinition.screenPaddingSmall,
+            right: DisplayDefinition.screenPaddingSmall,
+          ),
           itemBuilder: (_, index) {
             final playablePiece = pieces[index];
             final playStatus = playablePiece.status;
-            final thumbnail = playablePiece.piece.map(
+
+            final thumbnailImage = playablePiece.piece.map(
               generating: (_) => Container(),
               generated: (generated) =>
                   Image.network(generated.thumbnailUrl, fit: BoxFit.fitWidth),
             );
+
             const thumbnailHeight = 74.0;
-            final leading = Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                SizedBox(
-                  width: thumbnailHeight * DisplayDefinition.aspectRatio,
-                  height: thumbnailHeight,
-                  child: thumbnail,
-                ),
-                playStatus.when(
-                  stop: () => const Icon(Icons.play_arrow),
-                  playing: (_) => const Icon(Icons.stop),
-                ),
-              ],
+            final thumbnail = SizedBox(
+              width: thumbnailHeight * DisplayDefinition.aspectRatio,
+              height: thumbnailHeight,
+              child: thumbnailImage,
             );
 
             final piece = playablePiece.piece;
+            final foregroundColor = piece.map(
+              generating: (_) => Theme.of(context).disabledColor,
+              generated: (_) => null,
+            );
+            final nameText = Text(
+              piece.name,
+              style: TextStyle(color: foregroundColor),
+            );
+
             final dateFormatter = DateFormat.yMd('ja');
             final timeFormatter = DateFormat.Hm('ja');
-            final subtitleLabel = piece.map(
+            final detailsLabel = piece.map(
               generating: (generating) =>
                   '${dateFormatter.format(generating.submittedAt)} '
                   '${timeFormatter.format(generating.submittedAt)}   '
@@ -87,59 +94,87 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                   '${dateFormatter.format(generated.generatedAt)} '
                   '${timeFormatter.format(generated.generatedAt)}',
             );
+            final detailsText = Text(
+              detailsLabel,
+              style: TextStyle(color: foregroundColor),
+            );
 
-            final void Function()? onTap;
-            onTap = piece.map(
+            final onPressedShareButton = piece.map(
               generating: (_) => null,
-              generated: (generatedPiece) => playStatus.when(
-                stop: () => () {
-                  Navigator.push(
-                    context,
-                    VideoScreen.route(url: generatedPiece.movieUrl),
-                  );
-                },
-                playing: (_) => () => ref
-                    .read(widget.viewModel.notifier)
-                    .stop(piece: playablePiece),
-              ),
+              generated: (_) => () => _share(piece: piece),
+            );
+            final shareButton = IconButton(
+              icon: const Icon(Icons.share),
+              color: foregroundColor,
+              onPressed: onPressedShareButton,
             );
 
-            final tile = ListTile(
-              leading: Column(
-                children: [
-                  Expanded(child: leading),
-                ],
-              ),
-              title: Text(piece.name),
-              subtitle: Text(subtitleLabel),
-              trailing: IconButton(
-                icon: const Icon(Icons.share),
-                onPressed: () => _share(piece: piece),
-              ),
-              tileColor: piece.map(
-                generating: (_) => Colors.grey[300],
-                generated: (_) => null,
-              ),
-              onTap: onTap,
+            final onTap = piece.map(
+              generating: (_) => null,
+              generated: (generatedPiece) {
+                return playStatus.when(
+                  stop: () {
+                    return () {
+                      Navigator.push(
+                        context,
+                        VideoScreen.route(url: generatedPiece.movieUrl),
+                      );
+                    };
+                  },
+                  playing: (_) {
+                    return () => ref
+                        .read(widget.viewModel.notifier)
+                        .stop(piece: playablePiece);
+                  },
+                );
+              },
             );
 
-            return Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                tile,
-                playStatus.when(
-                  stop: () => const Visibility(
-                    visible: false,
-                    maintainState: true,
-                    maintainAnimation: true,
-                    maintainSize: true,
-                    child: LinearProgressIndicator(),
-                  ),
-                  playing: (position) => Visibility(
-                    child: LinearProgressIndicator(value: position),
+            final borderColor = piece.map(
+              generating: (_) => Theme.of(context).dividerColor,
+              generated: (_) => Colors.transparent,
+            );
+            final backgroundColor = piece.map(
+              generating: (_) => Colors.transparent,
+              generated: (_) => Theme.of(context).cardColor,
+            );
+
+            return ClipRRect(
+              borderRadius: const BorderRadius.all(
+                Radius.circular(
+                  DisplayDefinition.cornerRadiusSizeSmall,
+                ),
+              ),
+              child: Material(
+                color: backgroundColor,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: borderColor),
+                  borderRadius: BorderRadius.circular(
+                    DisplayDefinition.cornerRadiusSizeSmall,
                   ),
                 ),
-              ],
+                child: InkWell(
+                  onTap: onTap,
+                  child: Row(
+                    children: [
+                      thumbnail,
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            nameText,
+                            const SizedBox(height: 8),
+                            detailsText,
+                          ],
+                        ),
+                      ),
+                      shareButton,
+                      const SizedBox(width: 16),
+                    ],
+                  ),
+                ),
+              ),
             );
           },
           itemCount: pieces.length,
@@ -164,36 +199,32 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
 
     final scaffold = Scaffold(
       appBar: AppBar(
-        title: const Text('Meow Music'),
+        title: const Text('つくった作品'),
         actions: [
           _SettingsButton(
             onPressed: () => Navigator.push(context, SettingsScreen.route()),
           )
         ],
       ),
-      body: SafeArea(
-        bottom: false,
-        left: false,
-        right: false,
-        child: Column(
-          children: [
-            Expanded(
-              child: Stack(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: body,
-                  ),
-                  const Positioned(
-                    bottom: 0,
-                    left: 16,
-                    child: LyingDownCatImage(),
-                  ),
-                ],
-              ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                SafeArea(
+                  top: false,
+                  bottom: false,
+                  child: body,
+                ),
+                const Positioned(
+                  bottom: 0,
+                  left: 16,
+                  child: LyingDownCatImage(),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
