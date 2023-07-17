8bf0a2cac520b8121708d4838b886b02d5ad693b
shotaIDE <ide.shota.dev@gmail.com>
2023-07-17 10:22:31 +0900

    refactor

--- task/storage.py
+++ task/storage.py
@@ -6,7 +6,9 @@ from os.path import splitext
 from firebase_admin import storage
 
 from storage_rule import (TEMPLATE_EXTENSION, TEMPLATE_FILE_NAME,
-                          THUMBNAIL_FILE_NAME, USER_MEDIA_DIRECTORY_NAME)
+                          THUMBNAIL_FILE_NAME)
+
+_USER_MEDIA_RELATIVE_PARENT_PATH = 'userMedia'
 
 
 def download_template_bgm(template_id: str) -> str:
@@ -46,63 +48,63 @@ def upload_template_thumbnail(template_id: str, file_path: str):
 
 
 def download_unedited_user_media(uid: str, file_name: str) -> str:
-    splitted_file_name = splitext(file_name)
-    uploaded_extension = splitted_file_name[1]
-
     bucket = storage.bucket()
 
-    _, uploaded_local_base_path = tempfile.mkstemp()
-    uploaded_local_path = f'{uploaded_local_base_path}{uploaded_extension}'
+    splitted_file_name = splitext(file_name)
+    file_extension = splitted_file_name[1]
 
-    uploaded_relative_path = (
-        f'{USER_MEDIA_DIRECTORY_NAME}/{uid}/'
-        f'unedited/{file_name}'
+    _, local_base_path = tempfile.mkstemp()
+    local_path = f'{local_base_path}{file_extension}'
+
+    parent_relative_directory = (
+        f'{_USER_MEDIA_RELATIVE_PARENT_PATH}/{uid}/unedited'
     )
-    uploaded_blob = bucket.blob(uploaded_relative_path)
+    relative_path = f'{parent_relative_directory}/{file_name}'
+    blob = bucket.blob(relative_path)
 
-    uploaded_blob.download_to_filename(uploaded_local_path)
+    blob.download_to_filename(local_path)
 
-    return uploaded_local_path
+    return local_path
 
 
 def download_edited_user_media(uid: str, file_name: str) -> str:
-    _, sound_local_base_path = tempfile.mkstemp()
-    splitted_piece_movie_file_name = splitext(file_name)
-    sound_extension = splitted_piece_movie_file_name[1]
-    sound_local_path = f'{sound_local_base_path}{sound_extension}'
+    _, local_base_path = tempfile.mkstemp()
+    splitted_file_name = splitext(file_name)
+    file_extension = splitted_file_name[1]
+    local_path = f'{local_base_path}{file_extension}'
 
     bucket = storage.bucket()
 
-    sound_relative_path = (
-        f'{USER_MEDIA_DIRECTORY_NAME}/{uid}/'
-        f'edited/{file_name}'
+    parent_relative_directory = (
+        f'{_USER_MEDIA_RELATIVE_PARENT_PATH}/{uid}/edited'
     )
-    sound_blob = bucket.blob(sound_relative_path)
+    relative_path = f'{parent_relative_directory}/{file_name}'
+    blob = bucket.blob(relative_path)
 
-    sound_blob.download_to_filename(sound_local_path)
+    blob.download_to_filename(local_path)
 
-    return sound_local_path
+    return local_path
 
 
 def upload_piece_movie(uid: str, file_name: str, file_path: str):
     bucket = storage.bucket()
 
-    piece_movie_relative_path = (
-        f'{USER_MEDIA_DIRECTORY_NAME}/{uid}/'
-        f'generatedPieces/{file_name}'
+    parent_relative_directory = (
+        f'{_USER_MEDIA_RELATIVE_PARENT_PATH}/{uid}/generatedPieces'
     )
-    piece_movie_blob = bucket.blob(piece_movie_relative_path)
+    relative_path = f'{parent_relative_directory}/{file_name}'
+    blob = bucket.blob(relative_path)
 
-    piece_movie_blob.upload_from_filename(file_path)
+    blob.upload_from_filename(file_path)
 
 
 def upload_piece_thumbnail(uid: str, file_name: str, file_path: str):
     bucket = storage.bucket()
 
-    piece_thumbnail_relative_path = (
-        f'{USER_MEDIA_DIRECTORY_NAME}/{uid}/'
-        f'generatedThumbnail/{file_name}'
+    parent_relative_directory = (
+        f'{_USER_MEDIA_RELATIVE_PARENT_PATH}/{uid}/generatedThumbnail'
     )
-    piece_thumbnail_blob = bucket.blob(piece_thumbnail_relative_path)
+    relative_path = f'{parent_relative_directory}/{file_name}'
+    blob = bucket.blob(relative_path)
 
-    piece_thumbnail_blob.upload_from_filename(file_path)
+    blob.upload_from_filename(file_path)
