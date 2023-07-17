800c7a38f4fc6897e7f984adf5b71f9960528c4e
shotaIDE <ide.shota.dev@gmail.com>
2023-07-17 10:08:30 +0900

    fix

--- task/main.py
+++ task/main.py
@@ -211,8 +211,7 @@ def piece(request):
 
     current = datetime.now()
     piece_movie_base_name = f'{current.strftime("%Y%m%d%H%M%S")}_movie'
-    splitted_piece_movie_file_name = os.path.splitext(
-        piece_movie_path)
+    splitted_piece_movie_file_name = os.path.splitext(piece_movie_path)
     piece_movie_extension = splitted_piece_movie_file_name[1]
     piece_movie_file_name = f'{piece_movie_base_name}{piece_movie_extension}'
 
