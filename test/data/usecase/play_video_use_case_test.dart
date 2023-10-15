8ac8ba176661d1833f385ac0edae4281ab54326e
shotaIDE <ide.shota.dev@gmail.com>
2023-10-15 18:43:04 +0900

    test

--- test/data/usecase/play_video_use_case_test.dart
+++ test/data/usecase/play_video_use_case_test.dart
@@ -43,7 +43,7 @@ void main() {
       when(
         () => preferenceService
             .getInt(PreferenceKey.appCompletedToPlayVideoCount),
-      ).thenAnswer((_) async => 0);
+      ).thenAnswer((_) async => null);
 
       await providerContainer
           .read(onAppCompletedToPlayVideoActionProvider)
@@ -73,6 +73,177 @@ void main() {
         () => preferenceService.setInt(
           PreferenceKey.appCompletedToPlayVideoCount,
           value: 2,
+        ),
+      ).called(1);
+    });
+
+    test('Play count is 4, then skip request.', () async {
+      when(
+        () => preferenceService
+            .getInt(PreferenceKey.appCompletedToPlayVideoCount),
+      ).thenAnswer((_) async => 3);
+
+      await providerContainer
+          .read(onAppCompletedToPlayVideoActionProvider)
+          .call();
+
+      verifyNever(action.requestInAppReview);
+      verify(
+        () => preferenceService.setInt(
+          PreferenceKey.appCompletedToPlayVideoCount,
+          value: 4,
+        ),
+      ).called(1);
+    });
+
+    test('Play count is 5, then request.', () async {
+      when(
+        () => preferenceService
+            .getInt(PreferenceKey.appCompletedToPlayVideoCount),
+      ).thenAnswer((_) async => 4);
+
+      await providerContainer
+          .read(onAppCompletedToPlayVideoActionProvider)
+          .call();
+
+      verify(action.requestInAppReview).called(1);
+      verify(
+        () => preferenceService.setInt(
+          PreferenceKey.appCompletedToPlayVideoCount,
+          value: 5,
+        ),
+      ).called(1);
+    });
+
+    test('Play count is 6, then skip request.', () async {
+      when(
+        () => preferenceService
+            .getInt(PreferenceKey.appCompletedToPlayVideoCount),
+      ).thenAnswer((_) async => 5);
+
+      await providerContainer
+          .read(onAppCompletedToPlayVideoActionProvider)
+          .call();
+
+      verifyNever(action.requestInAppReview);
+      verify(
+        () => preferenceService.setInt(
+          PreferenceKey.appCompletedToPlayVideoCount,
+          value: 6,
+        ),
+      ).called(1);
+    });
+
+    test('Play count is 19, then skip request.', () async {
+      when(
+        () => preferenceService
+            .getInt(PreferenceKey.appCompletedToPlayVideoCount),
+      ).thenAnswer((_) async => 18);
+
+      await providerContainer
+          .read(onAppCompletedToPlayVideoActionProvider)
+          .call();
+
+      verifyNever(action.requestInAppReview);
+      verify(
+        () => preferenceService.setInt(
+          PreferenceKey.appCompletedToPlayVideoCount,
+          value: 19,
+        ),
+      ).called(1);
+    });
+
+    test('Play count is 20, then request.', () async {
+      when(
+        () => preferenceService
+            .getInt(PreferenceKey.appCompletedToPlayVideoCount),
+      ).thenAnswer((_) async => 19);
+
+      await providerContainer
+          .read(onAppCompletedToPlayVideoActionProvider)
+          .call();
+
+      verify(action.requestInAppReview).called(1);
+      verify(
+        () => preferenceService.setInt(
+          PreferenceKey.appCompletedToPlayVideoCount,
+          value: 20,
+        ),
+      ).called(1);
+    });
+
+    test('Play count is 21, then skip request.', () async {
+      when(
+        () => preferenceService
+            .getInt(PreferenceKey.appCompletedToPlayVideoCount),
+      ).thenAnswer((_) async => 20);
+
+      await providerContainer
+          .read(onAppCompletedToPlayVideoActionProvider)
+          .call();
+
+      verifyNever(action.requestInAppReview);
+      verify(
+        () => preferenceService.setInt(
+          PreferenceKey.appCompletedToPlayVideoCount,
+          value: 21,
+        ),
+      ).called(1);
+    });
+
+    test('Play count is 39, then skip request.', () async {
+      when(
+        () => preferenceService
+            .getInt(PreferenceKey.appCompletedToPlayVideoCount),
+      ).thenAnswer((_) async => 38);
+
+      await providerContainer
+          .read(onAppCompletedToPlayVideoActionProvider)
+          .call();
+
+      verifyNever(action.requestInAppReview);
+      verify(
+        () => preferenceService.setInt(
+          PreferenceKey.appCompletedToPlayVideoCount,
+          value: 39,
+        ),
+      ).called(1);
+    });
+
+    test('Play count is 40, then request.', () async {
+      when(
+        () => preferenceService
+            .getInt(PreferenceKey.appCompletedToPlayVideoCount),
+      ).thenAnswer((_) async => 39);
+
+      await providerContainer
+          .read(onAppCompletedToPlayVideoActionProvider)
+          .call();
+
+      verify(action.requestInAppReview).called(1);
+      verify(
+        () => preferenceService.setInt(
+          PreferenceKey.appCompletedToPlayVideoCount,
+          value: 40,
+        ),
+      ).called(1);
+    });
+
+    test('Play count is 41, then skip request.', () async {
+      when(
+        () => preferenceService
+            .getInt(PreferenceKey.appCompletedToPlayVideoCount),
+      ).thenAnswer((_) async => 40);
+
+      await providerContainer
+          .read(onAppCompletedToPlayVideoActionProvider)
+          .call();
+
+      verifyNever(action.requestInAppReview);
+      verify(
+        () => preferenceService.setInt(
+          PreferenceKey.appCompletedToPlayVideoCount,
+          value: 41,
         ),
       ).called(1);
     });
