142eebc96b594d1980c618a071dd47d675fd2dea
Shota Ide <shotaIDE@users.noreply.github.com>
2023-12-09 23:51:31 +0900

    dev: create Firebase project by Terraform (#483)

--- infra/main.tf
+++ infra/main.tf
@@ -1,18 +1,102 @@
-provider "google" {
-  project     = "colomney-my-pet-melody-dev"
-  region      = "asia-east1"
-  credentials = file("google-cloud-credentials.json")
+variable "google_project_id_suffix" {
+  type        = string
+  description = "ID suffix for GCP project."
 }
 
-resource "google_cloud_tasks_queue" "advanced_configuration" {
-  name     = "colomney-my-pet-melody-dev-service"
-  location = "asia-east1"
+variable "google_project_display_name_suffix" {
+  type        = string
+  description = "Display name suffix for GCP project."
+}
 
-  rate_limits {
-    max_concurrent_dispatches = 1
-  }
+variable "google_billing_account_id" {
+  type        = string
+  description = "Billing account ID to be associated with GCP project."
+}
 
-  retry_config {
-    max_attempts = 2
+variable "application_id_suffix" {
+  type        = string
+  description = "Application ID suffix for iOS and Android."
+}
+
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
   }
+}
+
+# Configures the provider to use the resource block's specified project for quota checks.
+provider "google-beta" {
+  user_project_override = true
+}
+
+# Configures the provider to not use the resource block's specified project for quota checks.
+# This provider should only be used during project creation and initializing services.
+provider "google-beta" {
+  alias                 = "no_user_project_override"
+  user_project_override = false
+}
+
+resource "google_project" "default" {
+  provider = google-beta.no_user_project_override
+
+  name            = "MyPetMelody${var.google_project_display_name_suffix}"
+  project_id      = "colomney-my-pet-melody${var.google_project_id_suffix}"
+  billing_account = var.google_billing_account_id
+
+  labels = {
+    "firebase" = "enabled"
+  }
+}
+
+resource "google_project_service" "default" {
+  provider = google-beta.no_user_project_override
+  project  = google_project.default.project_id
+  for_each = toset([
+    "cloudbilling.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "cloudtasks.googleapis.com",
+    "firebase.googleapis.com",
+    "serviceusage.googleapis.com",
+  ])
+  service = each.key
+
+  # Don't disable the service if the resource block is removed by accident.
+  disable_on_destroy = false
+}
+
+resource "google_firebase_project" "default" {
+  provider = google-beta
+  project  = google_project.default.project_id
+
+  depends_on = [
+    google_project_service.default
+  ]
+}
+
+resource "google_firebase_apple_app" "default" {
+  provider = google-beta
+
+  project      = google_project.default.project_id
+  display_name = "iOS"
+  bundle_id    = "ide.shota.colomney.MyPetMelody${var.application_id_suffix}"
+  team_id      = "4UGYN353AH"
+
+  depends_on = [
+    google_firebase_project.default,
+  ]
+}
+
+resource "google_firebase_android_app" "default" {
+  provider = google-beta
+
+  project      = google_project.default.project_id
+  display_name = "Android"
+  package_name = "ide.shota.colomney.MyPetMelody${var.application_id_suffix}"
+
+  depends_on = [
+    google_firebase_project.default,
+  ]
 }
---
+++ infra/task-queue.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project     = "colomney-my-pet-melody${var.google_project_id_suffix}"
+  region      = "asia-east1"
+  credentials = file("google-cloud-credentials.json")
+}
+
+resource "google_cloud_tasks_queue" "advanced_configuration" {
+  name     = "colomney-my-pet-melody${var.google_project_id_suffix}-service"
+  location = "asia-east1"
+
+  rate_limits {
+    max_concurrent_dispatches = 1
+  }
+
+  retry_config {
+    max_attempts = 2
+  }
+
+  depends_on = [
+    google_project_service.default
+  ]
+}
