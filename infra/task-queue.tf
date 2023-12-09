29f05742225b9464233636531d0be9329e9236c2
Shota Ide <shotaIDE@users.noreply.github.com>
2023-12-10 01:09:29 +0900

    dev: setup Firestore by Terraform (#484)

--- infra/task-queue.tf
+++ infra/task-queue.tf
@@ -1,12 +1,12 @@
 provider "google" {
   project     = "colomney-my-pet-melody${var.google_project_id_suffix}"
-  region      = "asia-east1"
+  region      = var.google_project_location
   credentials = file("google-cloud-credentials.json")
 }
 
 resource "google_cloud_tasks_queue" "advanced_configuration" {
   name     = "colomney-my-pet-melody${var.google_project_id_suffix}-service"
-  location = "asia-east1"
+  location = var.google_project_location
 
   rate_limits {
     max_concurrent_dispatches = 1
