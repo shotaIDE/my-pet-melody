5676b1b7b1f5dee2f126934528a6a0fc5d87a17c
Shota Ide <shotaIDE@users.noreply.github.com>
2024-01-10 22:49:25 +0900

    build: upgrade Python to 3.10.9 (#550)

--- function/detection.py
+++ function/detection.py
@@ -3,44 +3,8 @@
 import time
 from typing import Callable
 
-from inaSpeechSegmenter import Segmenter
 from pydub import AudioSegment, effects
 from pydub.silence import detect_nonsilent
-
-
-def detect_speech_or_music(sound_path: str) -> dict:
-    start_time = time.perf_counter()
-
-    sound = AudioSegment.from_file(sound_path)
-    duration_seconds: int = sound.duration_seconds
-    duration_milliseconds = int(round(duration_seconds, 3) * 1000)
-
-    segmenter = Segmenter(
-        vad_engine='sm', detect_gender=False, batch_size=128, energy_ratio=0.5)
-
-    segmentations = segmenter(sound_path)
-
-    print(f'Detected result(raw): {segmentations}')
-
-    non_silences_list: list[list[int]] = [
-        [int(segmentation[1] * 1000), int(segmentation[2] * 1000)]
-        for segmentation in segmentations
-        if (segmentation[0] == 'speech' or
-            segmentation[0] == 'music')
-    ]
-
-    print(f'Detected result(ms): {non_silences_list}')
-
-    result = {
-        'segments': non_silences_list,
-        'durationMilliseconds': duration_milliseconds,
-    }
-
-    end_time = time.perf_counter()
-    elapsed_time = end_time - start_time
-    print(f'Running time to detect non silence: {elapsed_time:.3f}s')
-
-    return result
 
 
 def detect_non_silence(sound_path: str) -> dict:
