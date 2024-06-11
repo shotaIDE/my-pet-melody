9154111490875624ba0119d4ab316f9d973a14fb
shotaIDE <ide.shota.dev@gmail.com>
2024-06-11 21:48:27 +0900

    fix

--- infra/service-account.tf
+++ infra/service-account.tf
@@ -1,28 +1,3 @@
-# resource "google_service_account" "firebase_admin" {
-#   project      = google_project.default.project_id
-#   account_id   = "firebase-adminsdk-zlkov"
-#   display_name = "firebase-adminsdk"
-#   description  = "Firebase Admin SDK Service Agent"
-# }
-
-# resource "google_project_iam_member" "firebase_admin_service_agent" {
-#   project = google_project.default.project_id
-#   role    = "roles/firebase.sdkAdminServiceAgent"
-#   member  = "serviceAccount:${google_service_account.firebase_admin.email}"
-# }
-
-# resource "google_project_iam_member" "firebase_admin_auth" {
-#   project = google_project.default.project_id
-#   role    = "roles/firebaseauth.admin"
-#   member  = "serviceAccount:${google_service_account.firebase_admin.email}"
-# }
-
-# resource "google_project_iam_member" "firebase_admin_" {
-#   project = google_project.default.project_id
-#   role    = "roles/firebaseauth.admin"
-#   member  = "serviceAccount:${google_service_account.firebase_admin.email}"
-# }
-
 resource "google_service_account" "cloud_tasks" {
   project      = google_project.default.project_id
   account_id   = "cloud-tasks"
