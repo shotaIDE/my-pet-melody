ae7f5f768f3b17281249a048aec89ae209b1f6fe
shotaIDE <ide.shota.dev@gmail.com>
2024-06-11 21:50:01 +0900

    fix

--- infra/auth.tf
+++ infra/auth.tf
@@ -11,3 +11,19 @@ resource "google_identity_platform_config" "auth" {
     google_project_service.default,
   ]
 }
+
+resource "google_identity_platform_project_default_config" "auth" {
+  provider = google-beta
+  project  = google_project.default.project_id
+  sign_in {
+    allow_duplicate_emails = false
+
+    anonymous {
+      enabled = true
+    }
+  }
+
+  depends_on = [
+    google_identity_platform_config.auth
+  ]
+}
