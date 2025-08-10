1d61a86f96f796f30d24f0c2c6d518e2c12e09be
Shota Ide <ide.shota.dev@gmail.com>
2025-08-10 22:58:27 +0900

    LoginError

--- lib/data/model/login_error.dart
+++ lib/data/model/login_error.dart
@@ -4,7 +4,7 @@ part 'login_error.freezed.dart';
 
 @freezed
 abstract class LoginError with _$LoginError {
-  const factory LoginError.cancelledByUser() = _LoginErrorCancelledByUser;
-  const factory LoginError.alreadyInUse() = _LoginErrorAlreadyInUse;
-  const factory LoginError.unrecoverable() = _LoginErrorUnrecoverable;
+  const factory LoginError.cancelledByUser() = LoginErrorCancelledByUser;
+  const factory LoginError.alreadyInUse() = LoginErrorAlreadyInUse;
+  const factory LoginError.unrecoverable() = LoginErrorUnrecoverable;
 }
